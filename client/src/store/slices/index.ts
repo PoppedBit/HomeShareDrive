51b1f9b001ab780afac291fcd25fb2aefca236a4
poppedbit <poppedbit@gmail.com>
2024-09-26 23:53:07 -0600

    fixed an oops

--- client/src/store/slices/index.ts
+++ client/src/store/slices/index.ts
@@ -1,3 +1,4 @@
 export { default as adminReducer } from './admin';
+export { default as homeshareReducer } from './homeshare';
 export { default as notificationsReducer } from './notifications';
 export { default as userReducer } from './user';
